9b17ecef3dc8d95bb541d229669150db58e6a284
Ashir Imran <71806327+xlinkfrostx@users.noreply.github.com>
2023-12-29 16:12:57 -0500

    feat: projects and about

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        darkblue: {
+          400: "#6f6f9b",
+          700: "#313154",
+          800: "#1a1a42",
+          900: "#0c0c1e",
+        },
+        lfred: {
+          300: "#f17ea1",
+          400: "#e75f88",
+          500: "#e13367",
+          600: "#c81e51",
+        },
+      },
+      screens: {
+        xs: "475px",
+      },
+    },
+  },
+  plugins: [],
+};
